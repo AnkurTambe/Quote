7e4852b9acc1b4f3e3f050ee98fd1d4d3d39e7b9
AnkurTambe <201951025@iiitvadodara.ac.in>
2021-01-05 16:37:41 +0530

    adding the folder

---
+++ Quote/app/src/main/java/com/example/quote/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.quote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this,"This is a programming quote",Toast.LENGTH_SHORT).show();
+    }
+}
